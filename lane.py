80e033b0a7d2ecb356d08e4f757d82ec5bfd9e88
rashad <rashadphilizaire@gmail.com>
2020-03-21 00:34:16 -0400

    reduced image noise

--- lane.py
+++ lane.py
@@ -3,6 +3,9 @@ import numpy as np
 
 img = cv2.imread("test_image.jpg")
 lane_img = np.copy(img)
-gray_img = cv2.cvtColor(lane_img, cv2.COLOR_RGB2GRAY)
-cv2.imshow("result", gray_img)
+
+gray_img = cv2.cvtColor(lane_img, cv2.COLOR_RGB2GRAY) #converts image to grayscale
+
+blur = cv2.GaussianBlur(gray_img, (5,5), 0) #GaussianBlur to reduce noise
+cv2.imshow("result", blur)
 cv2.waitKey(0)
