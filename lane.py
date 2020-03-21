a93c1f86f6d88daadeb0bf68f3e4e46cedbd586a
rashad <rashadphilizaire@gmail.com>
2020-03-21 00:39:43 -0400

    outline strong gradients

--- lane.py
+++ lane.py
@@ -7,5 +7,7 @@ lane_img = np.copy(img)
 gray_img = cv2.cvtColor(lane_img, cv2.COLOR_RGB2GRAY) #converts image to grayscale
 
 blur = cv2.GaussianBlur(gray_img, (5,5), 0) #GaussianBlur to reduce noise
-cv2.imshow("result", blur)
+
+canny = cv2.Canny(blur, 50, 150) #outlines strongest gradients in image
+cv2.imshow("result", canny)
 cv2.waitKey(0)
