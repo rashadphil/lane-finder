73e53e5f65fd13d3bcf026fb3c542ba1dd4644da
rashad <rashadphilizaire@gmail.com>
2020-03-21 01:43:25 -0400

    more accurate lines slopes

--- lane.py
+++ lane.py
@@ -1,6 +1,37 @@
 import cv2
 import numpy as np
 
+def make_coords(img, line_parameters):
+    slope, intercept = line_parameters
+    y1 = img.shape[0]
+    y2 = int(y1 * (3/5))
+    x1 = int((y1 - intercept)/slope)
+    x2 = int((y2 - intercept)/slope)
+    return np.array([x1, y1, x2, y2])
+
+
+
+def avg_slope_int(img, lines): #used to fill in unfilled lines
+    left_fit = []
+    right_fit = []
+    for line in lines:
+        x1, y1, x2, y2 = line.reshape(4)
+        parameters = np.polyfit((x1, x2), (y1, y2), 1)
+        slope = parameters[0]
+        intercept = parameters[1]
+        if slope <0:
+            left_fit.append((slope,intercept))
+        else:
+            right_fit.append((slope,intercept))
+    left_fit_avg = np.average(left_fit, axis = 0)
+    right_fit_avg = np.average(right_fit, axis =0)
+
+    left_line = make_coords(img, left_fit_avg)
+    right_line = make_coords(img, right_fit_avg)
+    return np.array([left_line, right_line])
+
+
+
 def canny(img):
     gray_img = cv2.cvtColor(lane_img, cv2.COLOR_RGB2GRAY) #converts image to grayscale
     blur = cv2.GaussianBlur(gray_img, (5,5), 0) #GaussianBlur to reduce noise
@@ -30,7 +61,9 @@ canny = canny(lane_img)
 
 cropped_img = region(canny)
 lines = cv2.HoughLinesP(cropped_img, 2, np.pi/180, 100, np.array([]), minLineLength= 40, maxLineGap=5) #detects straight lines in image
-lines_img = display_lines(lane_img, lines)
+avg_lines = avg_slope_int(lane_img, lines)
+
+lines_img = display_lines(lane_img, avg_lines)
 
 combo_img = cv2.addWeighted(lane_img, 0.8, lines_img, 1, 1) #combines line img and original img
 cv2.imshow("result", combo_img)
